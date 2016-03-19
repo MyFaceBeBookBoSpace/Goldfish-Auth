25a636f54bd596a0ad5a551ea4231b7d1faa709e
David Wood <david.p.wood@gmail.com>
2016-03-19 22:16:29 +0000

    Yeah

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var Pusher = require('pusher');
+
+var app = express();
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var pusher = new Pusher({
+  appId: process.env.PUSHER_APPID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET
+});
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
